c01b44cfe3d95e33dd3028c74e0beab0a0cc2527
Ismael Lucas de Leyssac <ismael1.lucas-de-leyssac@epitech.eu>
2025-09-22 20:18:01 +0200

    fix: regex in test 01 to match any system

--- tests/1/test_task01.py
+++ tests/1/test_task01.py
@@ -5,7 +5,7 @@ user = getpass.getuser()
 COMMAND = "ls -l --no-group task01"
 
 OUTPUT = f"""
-total 8
+total $*
 -rw-rw-r-- 1 {user}  0 $* test01
 -rwxrwxr-x 1 {user} 41 $* test02
 lrwxrwxrwx 1 {user}  6 $* test03 -> test02
